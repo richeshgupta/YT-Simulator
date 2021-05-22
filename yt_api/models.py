818febda2720bd6887abfb2ea748674df1535ddd
Richesh Gupta <rgrichesh45@gmail.com>
2021-05-22 16:31:26 +0530

    Db index = true for vid_id attr

--- yt_api/models.py
+++ yt_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class VideoData(models.Model):
-    vid_id = models.CharField(max_length=100)
+    vid_id = models.CharField(max_length=100,db_index=True)
     title = models.CharField(max_length=100)
     thumbnails = models.URLField(max_length=200)
     description = models.TextField()
